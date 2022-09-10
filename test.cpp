18154f9198d5501ae15049d33bf45ac0852e3ba8
Daniel Kim <chipperday@gmail.com>
2022-09-10 13:40:34 -0700

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void reverse(int nums[], int n) {
+
+  int L = 0;
+  int R = n - 1;
+
+  while (?) {
+    int t = nums[L];
+    nums[L] = nums[R];
+    nums[R] = t;
+
+    ?
+    ?
+  }
+}
+
+int main() {
+
+  int nums[5] = {4, 5, 6, 7, 8};
+  int n = 5;
+
+  reverse(nums, n);
+
+  for (int v : nums) {
+    cout << v << "\n";
+  }
+  cout << "\n";
+}
